8682a784a7d0c555861c0e45b7bdb9f47a5edfed
ricardomouraspl <ricardo.moura@superlogica.com>
2020-08-03 13:54:38 -0300

    fix cors

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const jsonServer = require('json-server');
-
+const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
@@ -7,6 +7,7 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
 server.use(middlewares);
+server.cors('*');
 server.use(router);
 server.listen(port, () => {
   // eslint-disable-next-line no-console
